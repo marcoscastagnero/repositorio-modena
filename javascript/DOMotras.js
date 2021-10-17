e27ea8cba06a0a3ff17c109f640c48c0137854f3
marcoscastagner@gmail.com <marcoscastagnero@gmail.com>
2021-10-16 22:35:57 -0300

    algunos avances

---
+++ javascript/DOMotras.js
@@ -0,0 +1,41 @@
+$("body").prepend(`
+<div class="head-index>
+            <div class="logo-index">
+                <a href="/index.html"><img src="/img/logo/logo-modena.png" alt="" width="90px" height="50px"></a>
+            <div class="sesion-index">
+                <nav>
+                    <ul>
+                        <li>Iniciar Sesión</li>
+                    </ul>
+                </nav>
+            </div>
+            </div>
+</div>
+`);
+
+/*
+<main>
+<h1 class="titulo-otras">NUESTROS DESTINOS</h1>
+        <div class="grid-culturas">
+            <div class="tailandia">
+                <img src="/img/seccion-culturas/tailandia.png" alt="" width="400px" height="400px">
+                <button type="button" class="boton-maceio">COMPRAR</button>
+            </div>
+            <div class="camboya">
+                <img src="/img/seccion-culturas/camboya.png" alt="" width="400px" height="400px">
+                <button type="button" class="boton-disney">COMPRAR</button>
+            </div>
+            <div class="namibia">
+                <img src="/img/seccion-culturas/namibia.png" alt="" width="400px" height="400px">
+                <button type="button" class="boton-salta">COMPRAR</button>
+            </div>
+            <div class="safari">
+                <img src="/img/seccion-culturas/safari.png" alt="" width="400px" height="400px">
+                <button type="button" class="boton-salta">COMPRAR</button>
+            </div>
+            <div class="ruta">
+                <img src="/img/seccion-culturas/ruta-gran-sur.png" alt="" width="400px" height="400px">
+                <button type="button" class="boton-salta">COMPRAR</button>
+            </div>
+        </div>
+</main>*/
